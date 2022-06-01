ea511382bba44711ecce3113c0ea877db870e07a
Jonny Vickers <jonathanbvickers@gmail.com>
2022-06-01 13:17:07 +0100

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-// 
+
+// COMPONENTS
+import Header from './components/Header'
+import Home from './components/Home'
+import PentaroWay from './components/PentaroWay'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
-      <>
-      </>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/pentaroway' element={<PentaroWay/>}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
   )
 }
 
