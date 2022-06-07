5391019652fb760dfb055907a08c2341331841e4
Jonny Vickers <jonathanbvickers@gmail.com>
2022-06-07 16:25:25 +0100

    contact form created

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// CHAKRA STYLING
+import { Button } from '@chakra-ui/react'
 
 const Contact = () => {
+
+  
+  // STATE
+  const [formData, setFormData] = useState({
+    full_name: '',
+    email: '',
+    company_name: '',
+    phone_number: '',
+    message: '',
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  }
+
+  const handleChange = (e) => {
+    const newObj = { ...formData, [e.target.name]: e.target.value}
+    setFormData(newObj)
+  }
   return (
     <section>
-      <h1>Contact</h1>
+      <h1>CONTACT US</h1>
+
+      <hr></hr>
+
+      <h2>GET IN TOUCH</h2>
+      <p>Have a project you need help with? Send us a message and we'll reply within X days.</p>
+      <form onSubmit={handleSubmit} className='contact-form'>
+
+        <div className='form-field'>
+          <label  htmlFor='full_name'>Full Name <span className='red'>*</span></label>
+          <input required type='text' name='full_name' placeholder='Full Name' value={formData.full_name} onChange={handleChange}/>
+        </div>
+
+
+        <div className='form-field'>
+          <label htmlFor='email'>Email <span className='red'>*</span></label>
+          <input required type='email' name='email' placeholder='email@email.com' value={formData.email} onChange={handleChange}/>
+        </div>
+
+        <div className='form-field'>
+          <label htmlFor='company_name'>Company Name <span className='red'>*</span></label>
+          <input required type='text' name='company_name' placeholder='Compnay Name Ltd.' value={formData.company_name} onChange={handleChange}/>
+        </div>
+
+        <div className='form-field'>
+          <label htmlFor='phone_number'>Phone Number <span className='red'>*</span></label>
+          <input required type='tel' name='phone_number' placeholder='0123456789' value={formData.phone_number} onChange={handleChange}/>
+        </div>
+
+        <div className='form-field message-area'>
+          <label htmlFor='message'>Message <span className='red'>*</span></label>
+          <textarea rows='6' required name='message' placeholder='Your message here...' value={formData.message} onChange={handleChange}/>
+        </div>
+
+        <Button className='light-btn' onClick={handleSubmit}>Send message</Button>
+
+      </form>
     </section>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 // STYLING
 import { Button } from '@chakra-ui/react'
 const Footer = () => {
   
+  // STATE
+  const [hideFooter, setHideFooter] = useState(false)
+
   const navigate = useNavigate()
 
-  // const handleSubmit = () => {
-  //   // console.log('working')
-  // }
+  // CHECK IF ON THE GETINTOUCH PAGE
+  useEffect(() => {
+    const url = window.location.href.split('/')
+    url.includes('getintouch') ? setHideFooter(true) : setHideFooter(false)
+  }, )
+  
   return (
-    <div className='footer-main'>
-      {/* <form onSubmit={handleSubmit}>
-        <label htmlFor=''
-      
-      </form> */}
-      <Button onClick={() => navigate('getintouch')} className='get-in-touch-button'>Send us a message</Button>
+    <div className={hideFooter ? 'footer-hiddent' : 'footer-main'}>
+      <div className='footer-mobile'>
+        <Button onClick={() => navigate('getintouch')} className='get-in-touch-button'>Send us a message</Button>
+      </div>
+      <div className='footer-desktop'>
+        <p>Destop</p>
+      </div>
     </div>
   )
 }
