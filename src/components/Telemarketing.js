96542e45ddf113bc65aeb59657bd7473a56514eb
Jonny Vickers <jonathanbvickers@gmail.com>
2022-06-22 14:52:10 +0100

    made split screen scrolling for desktop pentagon options

--- src/components/Telemarketing.js
+++ src/components/Telemarketing.js
@@ -37,167 +37,177 @@ const Telemarketing = () => {
         </div>
       </div>
 
-      <div className='grey-section-container' ref={pentRef}>
+      <div className='pentagon-screen-split-container'>
 
-        <h2 className='accent'>THE POWER OF 5</h2>
+        <div className='grey-section-container' ref={pentRef}>
 
-        <hr></hr>
+          <h2 className='accent'>THE POWER OF 5</h2>
 
-        <p>Here at Pentaro, we offer five distinct telemarketing services</p>
-        
-        <br />
-        <p>Proin fringilla augue at sapien imperdiet mollis. Vivamus ut quam ut tellus ullamcorper elementum non non turpis.</p>
-        
-        <br />
-        <p>Click to find out more below.</p>
+          <hr></hr>
 
-        <br />
-        
-        <div className='pentagon-flex-container'>
-          <div className='pentagon-clip-path-container'>
+          <p>Here at Pentaro, we offer five distinct telemarketing services</p>
+          
+          {/* <p>Click to find out more below.</p> */}
 
-            <h3 className='left-text' onClick={() => scrollTo(oneRef)}>ONE</h3>
-            <h3 className='right-text' onClick={() => scrollTo(twoRef)}>TWO</h3>
-            <h3 className='bot-left-text' onClick={() => scrollTo(threeRef)}>THREE</h3>
-            <h3 className='bot-right-text' onClick={() => scrollTo(fourRef)} >FOUR</h3>
-            <h3 className='bot-text' onClick={() => scrollTo(fiveRef)}>FIVE</h3>
+          <br />
 
-            <div onClick={() => scrollTo(oneRef)} className='triangle-container left-tri'><div className='blue-background'></div></div>
-            <div onClick={() => scrollTo(twoRef)} className='triangle-container right-tri'><div className='blue-background'></div></div>
-            <div onClick={() => scrollTo(threeRef)} className='triangle-container bot-left-tri'><div className='blue-background'></div></div>
-            <div onClick={() => scrollTo(fourRef)} className='triangle-container base-tri'><div className='blue-background'></div></div>
-            <div onClick={() => scrollTo(fiveRef)} className='triangle-container bot-right-tri'><div className='blue-background'></div></div>
+          <div className='pentagon-flex-container'>
+            <div className='pentagon-clip-path-container'>
 
+              <h3 className='left-text' onClick={() => scrollTo(oneRef)}>ONE</h3>
+              <h3 className='right-text' onClick={() => scrollTo(twoRef)}>TWO</h3>
+              <h3 className='bot-left-text' onClick={() => scrollTo(threeRef)}>THREE</h3>
+              <h3 className='bot-right-text' onClick={() => scrollTo(fourRef)} >FOUR</h3>
+              <h3 className='bot-text' onClick={() => scrollTo(fiveRef)}>FIVE</h3>
+
+              <div onClick={() => scrollTo(oneRef)} className='triangle-container left-tri'><div className='blue-background'></div></div>
+              <div onClick={() => scrollTo(twoRef)} className='triangle-container right-tri'><div className='blue-background'></div></div>
+              <div onClick={() => scrollTo(threeRef)} className='triangle-container bot-left-tri'><div className='blue-background'></div></div>
+              <div onClick={() => scrollTo(fourRef)} className='triangle-container base-tri'><div className='blue-background'></div></div>
+              <div onClick={() => scrollTo(fiveRef)} className='triangle-container bot-right-tri'><div className='blue-background'></div></div>
+
+
+            </div>
+          </div>
+
+            
+          </div>
+
+          <div className='service-options-container'>
+
+            <div className='service-option' ref={oneRef}>
+
+              <div className='content-container'>
+                <h2 className='accent'>SERVICE ONE</h2>
+                <hr></hr>
+                <h3>Proin fringilla augue at sapien</h3>
+                <p>Proin fringilla augue at sapien imperdiet mollis. Vivamus ut quam ut tellus ullamcorper elementum non non turpis.</p>
+                  
+                <p>Aenean sed sem at ante aliquam consequat. Ut rhoncus ex et orci feugiat, nec finibus neque rhoncus.</p>
+                
+                <button onClick={() => scrollTo(pentRef)}>Back to top</button>
+
+              </div>
+
+
+              <div className='image-container'>
+                <img 
+                  src={individual}
+                  alt='Man calling customers'
+                  />
+
+              </div>
+
+            </div>
+
+            <div className='service-option' ref={twoRef}>
+
+              <div className='content-container'>
+                <h2 className='accent'>SERVICE TWO</h2>
+                <hr></hr>
+                <h3>Proin fringilla augue at sapien</h3>
+                <p>Proin fringilla augue at sapien imperdiet mollis. Vivamus ut quam ut tellus ullamcorper elementum non non turpis.</p>
+                  
+                <p>Aenean sed sem at ante aliquam consequat. Ut rhoncus ex et orci feugiat, nec finibus neque rhoncus.</p>
+                
+                <button onClick={() => scrollTo(pentRef)}>Back to top</button>
+
+              </div>
+
+
+
+              <div className='image-container'>
+                <img 
+                  src={individual}
+                  alt='Man calling customers'
+                  />
+
+              </div>
+
+            </div>
+
+            <div className='service-option' ref={threeRef}>
+
+              <div className='content-container'>
+                <h2 className='accent'>SERVICE 3</h2>
+                <hr></hr>
+                <h3>Proin fringilla augue at sapien</h3>
+                <p>Proin fringilla augue at sapien imperdiet mollis. Vivamus ut quam ut tellus ullamcorper elementum non non turpis.</p>
+                  
+                <p>Aenean sed sem at ante aliquam consequat. Ut rhoncus ex et orci feugiat, nec finibus neque rhoncus.</p>
+                
+                <button onClick={() => scrollTo(pentRef)}>Back to top</button>
+
+              </div>
+
+
+
+              <div className='image-container'>
+                <img 
+                  src={individual}
+                  alt='Man calling customers'
+                  />
+
+              </div>
+
+            </div>
+
+            <div className='service-option' ref={fourRef}>
+
+              <div className='content-container'>
+                <h2 className='accent'>SERVICE 4</h2>
+                <hr></hr>
+                <h3>Proin fringilla augue at sapien</h3>
+                <p>Proin fringilla augue at sapien imperdiet mollis. Vivamus ut quam ut tellus ullamcorper elementum non non turpis.</p>
+                  
+                <p>Aenean sed sem at ante aliquam consequat. Ut rhoncus ex et orci feugiat, nec finibus neque rhoncus.</p>
+                
+                <button onClick={() => scrollTo(pentRef)}>Back to top</button>
+
+              </div>
+
+
+
+              <div className='image-container'>
+                <img 
+                  src={individual}
+                  alt='Man calling customers'
+                  />
+
+              </div>
+
+            </div>
+
+            <div className='service-option' ref={fiveRef}>
+
+              <div className='content-container'>
+                <h2 className='accent'>SERVICE 5</h2>
+                <hr></hr>
+                <h3>Proin fringilla augue at sapien</h3>
+                <p>Proin fringilla augue at sapien imperdiet mollis. Vivamus ut quam ut tellus ullamcorper elementum non non turpis.</p>
+                  
+                <p>Aenean sed sem at ante aliquam consequat. Ut rhoncus ex et orci feugiat, nec finibus neque rhoncus.</p>
+                
+                <button onClick={() => scrollTo(pentRef)}>Back to top</button>
+
+              </div>
+
+
+
+              <div className='image-container'>
+                <img 
+                  src={individual}
+                  alt='Man calling customers'
+                  />
+
+              </div>
+
+            </div>
 
           </div>
-        </div>
-        
-          
+
       </div>
 
-      <div className='dark-section-container' ref={oneRef}>
-
-        <div className='content-container'>
-          <h2 className='accent'>SERVICE ONE</h2>
-          <hr></hr>
-          <h3>Proin fringilla augue at sapien</h3>
-          <p>Proin fringilla augue at sapien imperdiet mollis. Vivamus ut quam ut tellus ullamcorper elementum non non turpis.</p>
-            
-          <p>Aenean sed sem at ante aliquam consequat. Ut rhoncus ex et orci feugiat, nec finibus neque rhoncus.</p>
-          
-        </div>
-
-        <button onClick={() => scrollTo(pentRef)}>Back to top</button>
-        
-        <div className='image-container'>
-          <img 
-            src={individual}
-            alt='Man calling customers'
-            />
-
-        </div>
-        
-      </div>
-
-      <div className='dark-section-container' ref={twoRef}>
-
-        <div className='content-container'>
-          <h2 className='accent'>SERVICE TWO</h2>
-          <hr></hr>
-          <h3>Proin fringilla augue at sapien</h3>
-          <p>Proin fringilla augue at sapien imperdiet mollis. Vivamus ut quam ut tellus ullamcorper elementum non non turpis.</p>
-            
-          <p>Aenean sed sem at ante aliquam consequat. Ut rhoncus ex et orci feugiat, nec finibus neque rhoncus.</p>
-          
-        </div>
-        
-        <button onClick={() => scrollTo(pentRef)}>Back to top</button>
-
-
-        <div className='image-container'>
-          <img 
-            src={individual}
-            alt='Man calling customers'
-            />
-
-        </div>
-        
-      </div>
-
-      <div className='dark-section-container' ref={threeRef}>
-
-        <div className='content-container'>
-          <h2 className='accent'>SERVICE 3</h2>
-          <hr></hr>
-          <h3>Proin fringilla augue at sapien</h3>
-          <p>Proin fringilla augue at sapien imperdiet mollis. Vivamus ut quam ut tellus ullamcorper elementum non non turpis.</p>
-            
-          <p>Aenean sed sem at ante aliquam consequat. Ut rhoncus ex et orci feugiat, nec finibus neque rhoncus.</p>
-          
-        </div>
-
-        <button onClick={() => scrollTo(pentRef)}>Back to top</button>
-
-        
-        <div className='image-container'>
-          <img 
-            src={individual}
-            alt='Man calling customers'
-            />
-
-        </div>
-        
-      </div>
-
-      <div className='dark-section-container' ref={fourRef}>
-
-        <div className='content-container'>
-          <h2 className='accent'>SERVICE 4</h2>
-          <hr></hr>
-          <h3>Proin fringilla augue at sapien</h3>
-          <p>Proin fringilla augue at sapien imperdiet mollis. Vivamus ut quam ut tellus ullamcorper elementum non non turpis.</p>
-            
-          <p>Aenean sed sem at ante aliquam consequat. Ut rhoncus ex et orci feugiat, nec finibus neque rhoncus.</p>
-          
-        </div>
-
-        <button onClick={() => scrollTo(pentRef)}>Back to top</button>
-
-        
-        <div className='image-container'>
-          <img 
-            src={individual}
-            alt='Man calling customers'
-            />
-
-        </div>
-        
-      </div>
-
-      <div className='dark-section-container' ref={fiveRef}>
-
-        <div className='content-container'>
-          <h2 className='accent'>SERVICE 5</h2>
-          <hr></hr>
-          <h3>Proin fringilla augue at sapien</h3>
-          <p>Proin fringilla augue at sapien imperdiet mollis. Vivamus ut quam ut tellus ullamcorper elementum non non turpis.</p>
-            
-          <p>Aenean sed sem at ante aliquam consequat. Ut rhoncus ex et orci feugiat, nec finibus neque rhoncus.</p>
-          
-        </div>
-
-        <button onClick={() => scrollTo(pentRef)}>Back to top</button>
-
-        
-        <div className='image-container'>
-          <img 
-            src={individual}
-            alt='Man calling customers'
-            />
-
-        </div>
-        
-      </div>
     </section>
   )
 }
