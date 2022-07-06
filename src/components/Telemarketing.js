22ee53b6474f3321cef57fa37abad79b87a7191b
Jonny Vickers <jonathanbvickers@gmail.com>
2022-07-06 18:07:31 +0100

    fixed wheel logo options

--- src/components/Telemarketing.js
+++ src/components/Telemarketing.js
@@ -2,7 +2,7 @@ import React, {useEffect, useRef, useState} from 'react'
 
 // IMAGES
 import individual from '../assets/telemarketing-individual.jpg'
-import logo from '../assets/whatsapp.jpeg'
+import logo from '../assets/logo-screenshot.png'
 
 const Telemarketing = () => {
 
@@ -36,48 +36,50 @@ const Telemarketing = () => {
         <div className='section-flex-container'>
 
           <h1 className='accent'>TELEMARKETING</h1>
-          <h2>Proin fringilla augue at sapien imperdiet mollis. Vivamus ut quam ut tellus ullamcorper elementum non non turpis.</h2>
+          <h2>Telemarketing at pentaro priorities quality, clarity, and consistency, producing direct connections and opportunities with your target audience.</h2>
         </div>
       </div>
 
       <div className='pentagon-screen-split-container'>
 
-        <div className='green-section-container' ref={pentRef}>
+        <div className='plain-section-container wheel' ref={pentRef}>
 
-          <h2 >THE POWER OF 5</h2>
+          <div className='content-container wheel'>
+            <h2 className='accent'>THE POWER OF 5</h2>
 
-          <hr></hr>
+            <hr></hr>
 
-          <p>Here at Pentaro, we offer five distinct telemarketing services</p>
-          
-          {/* <p>Click to find out more below.</p> */}
+            <p>Here at Pentaro, we offer five distinct telemarketing services</p>
 
-          <br />
-          <div className='logo-wheel-container'>
+            {/* <p>Click to find out more below.</p> */}
 
-            <img src={logo} alt='pentaro logo' />
+            <div className='logo-wheel-container'>
 
-            <div className='wheel-option top-left' onClick={() => scrollTo(oneRef)}><h3 onClick={() => scrollTo(oneRef)}>TELEMARKETING</h3></div>
-            <div className='wheel-option top-right'onClick={() => scrollTo(twoRef)}><h3 onClick={() => scrollTo(twoRef)}>CUSTOMER <br /> SERVICE</h3></div>
-            <div className='wheel-option right' onClick={() => scrollTo(threeRef)}><h3 onClick={() => scrollTo(threeRef)}>MARKET <br /> RESEARCH <br /> & DATA <br /> CLEANSING</h3></div>
-            <div className='wheel-option bottom' onClick={() => scrollTo(fourRef)}><h3 onClick={() => scrollTo(fourRef)}>LEAD <br /> GENERATION</h3></div>
-            <div className='wheel-option left' onClick={() => scrollTo(fiveRef)}><h3 onClick={() => scrollTo(fiveRef)}>APPOINTMENT <br /> SETTING</h3></div>
+              <img src={logo} alt='pentaro logo' />
 
+              <div className='wheel-option top-left' onClick={() => scrollTo(oneRef)}><h3 onClick={() => scrollTo(oneRef)}>TELE- <br /> MARKETING</h3></div>
+              <div className='wheel-option top-right'onClick={() => scrollTo(twoRef)}><h3 onClick={() => scrollTo(twoRef)}>CUSTOMER <br /> SERVICE</h3></div>
+              <div className='wheel-option right' onClick={() => scrollTo(threeRef)}><h3 onClick={() => scrollTo(threeRef)}>MARKET <br /> RESEARCH {/*<br /> & DATA <br /> CLEANSING */}</h3></div>
+              <div className='wheel-option bottom' onClick={() => scrollTo(fourRef)}><h3 onClick={() => scrollTo(fourRef)}>LEAD <br /> GENERATION</h3></div>
+              <div className='wheel-option left' onClick={() => scrollTo(fiveRef)}><h3 onClick={() => scrollTo(fiveRef)}>SCHEDULING</h3></div>
+
+            </div>
           </div>
+
+          
 
             
           </div>
 
-          <div className='service-options-container'>
+          <div className='plain-section-container service-option-container'>
 
-            <div className='service-option white-section-container' ref={oneRef}>
-
-              <div className='content-container'>
+            <div className='content-container service-option' ref={oneRef}>
+              <div className='dotted-line offset-left-top'></div>
                 <h2 className='accent'>TELEMARKETING</h2>
                 <hr></hr>
-                <h3>Telemarketing at pentaro priorities quality, clarity, and consistency, producing direct connections and opportunities with your target audience.</h3>
+                {/* <h3>Telemarketing at pentaro priorities quality, clarity, and consistency, producing direct connections and opportunities with your target audience.</h3> */}
                 
-                <p>Our strategic approach and emphasis on analysis delivers a service that sets us apart from our competitors.</p>
+                <h3>Our strategic approach and emphasis on analysis delivers a service that sets us apart from our competitors.</h3>
                   
                 <p>Effective communication is a key component in all of our processes, with frequent updates, daily statistics, and with constant availability to discuss progress in order to ensure results.</p>
                 
@@ -86,9 +88,8 @@ const Telemarketing = () => {
                 
                 <p>Every campaign Pentaro take on, regardless it’s size, will receive the same level of service to gain the best possible returns. Our ambition is to build productive relationships and share success.</p>
                 
-                <button onClick={() => scrollTo(pentRef)}>Back to top</button>
+                <button onClick={() => scrollTo(pentRef)}>Back to top <span className='accent'>^</span></button>
 
-              </div>
 
 
               {/* <div className='image-container'>
@@ -101,11 +102,18 @@ const Telemarketing = () => {
 
             </div>
 
-            <div className='service-option dark-container customer-service' ref={twoRef}>
+            <div className='image-container'>
+                <img 
+                  src={individual}
+                  alt='Man calling customers'
+                  />
 
+              </div>
+
+            <div className='content-container service-option' ref={twoRef}>
+                        <div className='dotted-line offset-left-top'></div>
               {/* <div className='dark-background customer-service-background'></div> */}
 
-              <div className='content-container'>
                 <h2 className='accent'>CUSTOMER SERVICE</h2>
                 <hr></hr>
                 <h3>Building and maintaining relationships with your customers is our speciality. </h3>
@@ -114,9 +122,8 @@ const Telemarketing = () => {
                   
                 <p>Our customer service contact centre reflects your company ethos at every point of contact to ensure a satisfied customer base – our team always applies a methodical approach to customer service. </p>
                 
-                <button onClick={() => scrollTo(pentRef)}>Back to top</button>
+                <button onClick={() => scrollTo(pentRef)}>Back to top <span className='accent'>^</span></button>
 
-              </div>
 
 
 
@@ -130,9 +137,16 @@ const Telemarketing = () => {
 
             </div>
 
-            <div className='service-option' ref={threeRef}>
+            <div className='image-container'>
+                <img 
+                  src={individual}
+                  alt='Man calling customers'
+                  />
 
-              <div className='content-container'>
+              </div>
+
+            <div className='content-container service-option' ref={threeRef}>
+                        <div className='dotted-line offset-left-top'></div>
                 <h2 className='accent'>MARKET RESEARCH & DATA CLEANSING</h2>
                 <hr></hr>
                 <h3>At Pentaro, our trained market research specialists implement bespoke strategies for each individual campaign.  Using market trends, direct research, process development, and risk analysis, we ensure the best insight into the market of your designated industry.</h3>
@@ -149,25 +163,24 @@ const Telemarketing = () => {
                   
                 <p>Information and market knowledge is invaluable to the success and growth for businesses in the current economic world. Market research and data cleansing through telemarketing is a fast and efficient method of delving deeper into your target audience, and the needs and desires of your customers.</p>
                 
-                <button onClick={() => scrollTo(pentRef)}>Back to top</button>
-
-              </div>
+                <button onClick={() => scrollTo(pentRef)}>Back to top <span className='accent'>^</span></button>
 
 
 
-              {/* <div className='image-container'>
+
+            </div>
+
+            <div className='image-container'>
                 <img 
                   src={individual}
                   alt='Man calling customers'
                   />
 
-              </div> */}
+              </div>
 
-            </div>
 
-            <div className='service-option' ref={fourRef}>
-
-              <div className='content-container'>
+            <div className='content-container service-option' ref={fourRef}>
+                        <div className='dotted-line offset-left-top'></div>
                 <h2 className='accent'>LEAD GENERATION</h2>
                 <hr></hr>
                 <h3>Our lead generation service unearths and cultivates businesses within your target market, in order to collect a wide range of information to streamline your sales process, enhancing your sales activity.</h3>
@@ -176,13 +189,15 @@ const Telemarketing = () => {
                   
                 <p>Our heavily analytical style utilises the best methods currently available, to ensure a constant and consistent flow of new business leads to nurture.</p>
                 
-                <button onClick={() => scrollTo(pentRef)}>Back to top</button>
-
-              </div>
+                <button onClick={() => scrollTo(pentRef)}>Back to top <span className='accent'>^</span></button>
 
 
 
-              <div className='image-container'>
+
+
+            </div>
+
+            <div className='image-container'>
                 <img 
                   src={individual}
                   alt='Man calling customers'
@@ -190,12 +205,9 @@ const Telemarketing = () => {
 
               </div>
 
-            </div>
-
-            <div className='service-option' ref={fiveRef}>
-
-              <div className='content-container'>
-                <h2 className='accent'>APPOINTMENT SETTING</h2>
+            <div className='content-container service-option' ref={fiveRef}>
+                        <div className='dotted-line offset-left-top'></div>
+                <h2 className='accent'>SCHEDULING & APPOINTMENT SETTING</h2>
                 <hr></hr>
                 <h3>At Pentaro, our appointment setting service generates high quality pre-booked meetings, with relevant key decision makers for your sales development team to attend.</h3>
 
@@ -205,9 +217,8 @@ const Telemarketing = () => {
                 
 
                 <p>Your telemarketer provides information packs detailing every appointment made, the key decision maker, and all buying signals or pain points that have been identified.</p>
-                <button onClick={() => scrollTo(pentRef)}>Back to top</button>
+                <button onClick={() => scrollTo(pentRef)}>Back to top <span className='accent'>^</span></button>
 
-              </div>
 
 
 
