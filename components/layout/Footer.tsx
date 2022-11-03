e481bc0d996bbcd75e5a313e3bbe296fa55affe2
Jonny Vickers <jonathanbvickers@gmail.com>
2022-11-03 17:03:54 +0000

    styling of footer matched

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="real-footer-container">
-      <div className="footer-links-container">
+    <footer className="footer__container">
+      <div className="footer__links-container">
         <Link href="/">Pentaro</Link>
         <Link href="/recruitment">Recruitment</Link>
         <Link href="/telemarketing">Telemarketing</Link>
@@ -11,10 +11,10 @@ const Footer = () => {
         <Link href="/get-in-touch">Contact Us</Link>
       </div>
 
-      <p>&copy; Pentaro Ltd. 2022</p>
-      <p>
+      <p className="paragraph--footer">&copy; Pentaro Ltd. 2022</p>
+      <p className="paragraph--footer">
         Website provided by{' '}
-        <a href="www.jonnyvickers.co.uk" target="_blank">
+        <a href="https://www.jonnyvickers.co.uk" target="_blank">
           Jonny Vickers
         </a>
       </p>
